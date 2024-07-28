2cc64cd5a97ac35fd481705c9c962714383438c3
Glowja <gloryjibril433@gmail.com>
2024-07-28 21:17:01 +0100

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Setup Event Listener for Page Load
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     // Select DOM Elements
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
